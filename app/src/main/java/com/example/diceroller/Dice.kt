66c7786de4217b4f5e10b909d9ec46a821add23b
matheus jordan <matheusj1599@gmail.com>
2021-03-15 22:43:38 -0300

    Finalizado Dice Roller a

--- app/src/main/java/com/example/diceroller/Dice.kt
+++ app/src/main/java/com/example/diceroller/Dice.kt
@@ -1,6 +1,8 @@
 package com.example.diceroller
 
 class Dice(private val numSides: Int) {
+    val luckyNumber = (1..numSides).random()
+
     fun roll(): Int {
         return (1..numSides).random()
     }
--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.diceroller
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -36,5 +39,60 @@ class MainActivity : AppCompatActivity() {
         resultDice1.text = dice1Face.toString()
         resultDice2.text = dice2Face.toString()
         resultDice.text = diceSum.toString()
+
+        val resultLucky1: TextView = findViewById(R.id.luckyResult1)
+        val resultLucky2: TextView = findViewById(R.id.luckyResult2)
+
+        val dice1LuckyNumber = dice1.luckyNumber
+        val dice2LuckyNumber = dice2.luckyNumber
+
+        resultLucky1.text = dice1LuckyNumber.toString()
+        resultLucky2.text = dice2LuckyNumber.toString()
+
+        val fullLucky = (dice1Face ==  dice1LuckyNumber) && (dice2Face == dice2LuckyNumber)
+        val halfLucky = (dice1Face ==  dice1LuckyNumber) || (dice2Face == dice2LuckyNumber)
+        val noneLucky = (dice1Face !=  dice1LuckyNumber) || (dice2Face != dice2LuckyNumber)
+
+        val resultLuckyText: TextView = findViewById(R.id.luckyText)
+
+        val luckyText = when(true) {
+            fullLucky -> {
+                resultLuckyText.setTextColor(Color.MAGENTA)
+                "Are you very lucky today! :D"
+            }
+            halfLucky -> {
+                resultLuckyText.setTextColor(Color.GREEN)
+                "Are you a bit lucky! :)"
+            }
+            noneLucky -> {
+                resultLuckyText.setTextColor(Color.BLACK)
+                "Nothing for today :/"
+            }
+            else -> {
+                resultLuckyText.setTextColor(Color.RED)
+                "oh man... !!! :'C"
+            }
+        }
+
+        resultLuckyText.text = luckyText
+
+        val dice1Thumbnail: ImageView = findViewById(R.id.face1Thumbnail)
+        val dice2Thumbnail: ImageView = findViewById(R.id.face2Thumbnail)
+
+        this.setDiceThumbnail(dice1Face, dice1Thumbnail)
+        this.setDiceThumbnail(dice2Face, dice2Thumbnail)
+    }
+
+    private fun setDiceThumbnail(diceNumber: Int, imageView: ImageView) {
+        val drawable = when (diceNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        imageView.setImageResource(drawable)
     }
 }
